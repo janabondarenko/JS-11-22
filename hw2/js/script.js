c153dba9f05f35b6aeb3d36f9400613d60aa3b6d
janabondarenko <110444969+janabondarenko@users.noreply.github.com>
2022-11-09 14:44:07 +0200

    Add files via upload

---
+++ hw2/js/script.js
@@ -0,0 +1,33 @@
+
+
+let userName = 'Jana';
+console.log(userName)
+
+let userSurname = 'Bondarenko';
+console.log(userSurname)
+
+let userAge;
+userAge = '29'
+
+console.log('userAge', userAge)
+
+// переменной, которая хранит нового пользователя
+
+let userNew
+
+// переменной, которая хранит список покупок
+
+let shoppingList
+
+
+// переменной, которая хранит информацию о том, является ли пользователь в сети (online)
+
+let userOnline
+
+// переменной, которая хранит зарплату работника
+
+let employeeSalary
+
+// переменной, которая хранит координаты курсора (мыши)
+
+let coordinatesCursor
