db678052f3bcc46c7017612a4e333a949fb023f8
janabondarenko <110444969+janabondarenko@users.noreply.github.com>
2022-11-18 20:52:15 +0200

    Add files via upload

---
+++ hw3/EX 1/script.js
@@ -0,0 +1,22 @@
+const a = 5;
+const b = 10;
+
+// сложение 
+
+operationResult = a + b;
+console.log (" 5 + 10 = " , operationResult);
+
+// вычитание 
+
+operationResult = a - b;
+console.log (" 5 - 10 = " , operationResult);
+
+// деление 
+
+operationResult = a / b;
+console.log (" 5 / 10 = " , operationResult);
+
+// умножение 
+
+operationResult = a * b;
+console.log (" 5 * 10 = " , operationResult);
---
+++ hw3/EX3/js/script.js
@@ -0,0 +1,14 @@
+let firstNumber = Number(prompt ("Введіть перше число"));
+let secondNumber = Number(prompt ("Введіть друге число"));
+  if (firstNumber > secondNumber){
+    alert('Число ' +firstNumber+ ' більше, ніж число '
+  +secondNumber)};
+
+  if (firstNumber < secondNumber) {
+    alert (" друге число більше за друге")};
+
+  if  (firstNumber === secondNumber) {
+    alert ("числа рівні")};
+
+
+    
