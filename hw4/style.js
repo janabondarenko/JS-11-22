cf65e838239a1f049879d7920b749b8dc16b8b6f
janabondarenko <110444969+janabondarenko@users.noreply.github.com>
2022-11-22 16:36:15 +0200

    Add files via upload

---
+++ hw4/style.js
@@ -0,0 +1,69 @@
+// задание 1
+
+function getComparison(a, b){
+  
+if (a === b) 
+  return true ;
+
+else ('a !=== b')
+  return false;
+}
+
+console.log('getComparison(4, 4)', getComparison(4, 4)); // true
+console.log('getComparison(10, 4)', getComparison(10, 4)); // false
+
+
+
+
+// задание 2
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log (sum(3,4)); // 7
+
+// стрелочний варіант
+
+      // const sum = (a, b) => a + b;
+
+      // const res2 = sum(4, 5)
+      // console.log(`res2`);
+
+
+
+// Задание 3
+
+// function getDeere (a, b = 2) {
+//   return a  **  b;
+// }
+// console.log(getDeere(5)) //25
+// console.log(getDeere(5, 3)) //125
+
+
+// Задание 4
+
+  // switch ('1') {
+  //   case '1':
+  //     console.log('Понедельник');
+  //     break;
+  //   case '2':
+  //     console.log('Вторник');
+  //     break;
+  //   case '3':
+  //     console.log('Среда');
+  //     break;
+  //   case '4':
+  //     console.log('Четверг');
+  //     break;
+  //   case '5':
+  //     console.log('Пятница');
+  //     break;
+  //   case '6':
+  //     console.log('Суббота');
+  //     break;
+  //   case '7':
+  //     console.log('Воскресенье');
+  //     break;
+  //   default:
+  //     console.log(`Такого дня нет`);
+  // }
